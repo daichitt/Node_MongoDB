e5469bee515bdedd0a573d701de9228222fd50a3
daichitt <gene.sixteen.000925@outlook.jp>
2023-02-03 19:12:14 +0900

    mongoose.connect

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 const mondoose = require("mongoose");
 
+mongoose.connect(
+    "mongodb+srv://morostagrm:XLqdSkFK2rnKj!z@cluster0.lp4hqwl.mongodb.net/Food?retryWrites=true&w=majority"
+)
+.then(() => console.log("接続に成功しました"))
+.catch((err) => console.log(err));
+
+console.log(mongoose.connection.readyState);
 app.listen(3000,() => {
     console.log("sucefully open sever ")
 })
